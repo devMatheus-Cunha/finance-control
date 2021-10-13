9959f56e593792d08561876ab68dd639dfc0ced9
Matheus Gomes <gomesprogramacao@gmail.com>
2021-10-13 20:47:56 -0300

    feat: sctructure for Component Content

---
+++ src/components/Content/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+// styles
+import { Container } from "./styles";
+
+const Aside: React.FC = () => {
+	return (
+		<Container>
+			Olá
+		</Container>
+	);
+}
+
+export default Aside;
