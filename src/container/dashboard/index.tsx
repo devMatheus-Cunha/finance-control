6b0bdaf4f10ad0964a707f5d78ebc204188cac90
Matheus Gomes <gomesprogramacao@gmail.com>
2021-10-13 21:13:41 -0300

    feat: applying structure templates in the dashborad

--- src/container/dashboard/index.tsx
+++ src/container/dashboard/index.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 
+// temmplates
+import Aside from "../../templates/Aside";
+import MainHeader from "../../templates/MainHeader";
+import Content from "../../templates/Content";
+
 // styles
-// import { Container } from './styles';
+import { Grid } from "./styles";
 
 const DashboardContainer: React.FC = () => {
-	return <h1>DashboardContainer</h1>;
+	return (
+		<Grid>
+			<MainHeader />
+			<Aside />
+			<Content />
+		</Grid>
+	);
 }
 
 export default DashboardContainer;
