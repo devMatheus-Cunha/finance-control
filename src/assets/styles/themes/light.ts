f960ce08795d6ab6f04162ca97d78b2b4143abc3
Matheus Gomes <gomesprogramacao@gmail.com>
2021-10-14 20:08:25 -0300

    feat: add theme dark and light

---
+++ src/assets/styles/themes/light.ts
@@ -0,0 +1,16 @@
+export default {
+	title: "light",
+	colors: {
+		primary: "#DCDCDC",
+		secondary: "#FFFFFF",
+		tertiary: "#F5F5F5",
+
+		black: "#fff",
+		white: "#000",
+		gray: "#bfbfbf",
+
+		success: "#03BB85",
+		info: "#3B5998",
+		warning: "#FF6961",
+	},
+};
