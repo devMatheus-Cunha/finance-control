48a49ae329f08b76e05e442108599836b8aa533d
Matheus Gomes <gomesprogramacao@gmail.com>
2021-10-18 12:33:16 -0300

    feat: structure component Toogle

---
+++ src/components/Toggle/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+// styles
+import { Container, ToggleLabel, ToggleSelector } from "./styles";
+
+const Toggle = () => (
+	<Container>
+		<ToggleLabel>
+			Light
+		</ToggleLabel>
+		<ToggleSelector
+			uncheckedIcon={false}
+			checkedIcon={false}
+			checked={false}
+			onChange={() => {}}
+		/>
+		<ToggleLabel>
+			Dark
+		</ToggleLabel>
+	</Container>
+);
+
+export default Toggle;
