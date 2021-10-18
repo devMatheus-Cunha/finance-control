48040569067d5689b05e409f82a8a61dd555a8bc
Matheus Gomes <gomesprogramacao@gmail.com>
2021-10-18 15:34:03 -0300

    feat: add structure template Aside

--- src/templates/Aside/index.tsx
+++ src/templates/Aside/index.tsx
@@ -1,14 +1,62 @@
 import React from "react";
 
+// icons
+import {
+	MdDashboard,
+	MdArrowDownward,
+	MdArrowUpward,
+	MdExitToApp,
+} from "react-icons/md";
+
+// images
+import logoImg from "../../assets/img/logo.svg";
+
 // styles
-import { Container } from "./styles";
+import {
+	Container,
+	Header,
+	LogoImg,
+	MenuContainer,
+	MenuItemLink,
+	Title,
+} from "./styles";
 
 const Aside: React.FC = () => {
 	return (
 		<Container>
-			Aside
+			<Header>
+				<LogoImg src={logoImg} alt="Logo Finance Control" />
+				<Title>Finance Control</Title>
+			</Header>
+			<MenuContainer>
+
+				<MenuItemLink href="#">
+					<MdDashboard />
+					Dashboard
+				</MenuItemLink>
+
+				<MenuItemLink href="#">
+
+					Novo registro
+				</MenuItemLink>
+
+				<MenuItemLink href="#">
+					<MdArrowUpward />
+					Entradas
+				</MenuItemLink>
+
+				<MenuItemLink href="#">
+					<MdArrowDownward />
+					Saídas
+				</MenuItemLink>
+
+				<MenuItemLink href="#">
+					<MdExitToApp />
+					Sair
+				</MenuItemLink>
+			</MenuContainer>
 		</Container>
 	);
-}
+};
 
 export default Aside;
