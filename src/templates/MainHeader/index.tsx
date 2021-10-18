579014be9c4f66bbc2c9b63521c00046e86b5728
Matheus Gomes <gomesprogramacao@gmail.com>
2021-10-18 11:27:17 -0300

    feat: structure for template MainHeader

--- src/templates/MainHeader/index.tsx
+++ src/templates/MainHeader/index.tsx
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useMemo } from "react";
+
+// utils
+import emojis from "../../utils/emojis";
 
 // styles
-import { Container } from "./styles";
+import {
+	Container, Profile, UserName, Welcome,
+} from "./styles";
 
 const MainHeader: React.FC = () => {
+	const emoji = useMemo(() => {
+		const indice = Math.floor(Math.random() * emojis.length);
+		return emojis[indice];
+	}, []);
+
 	return (
 		<Container>
-			mainHeader
+			<h3>TOggle</h3>
+			<Profile>
+				<Welcome>
+					Olá,
+					{" "}
+					{emoji}
+				</Welcome>
+				<UserName> Matheus Cunha</UserName>
+			</Profile>
 		</Container>
 	);
-}
+};
 
 export default MainHeader;
