19c854ee7f79c1e0f290a5e542215cd68768fc33
Matheus Gomes <gomesprogramacao@gmail.com>
2021-10-18 11:27:10 -0300

    feat: styles for template MainHeader

--- src/templates/MainHeader/styles.ts
+++ src/templates/MainHeader/styles.ts
@@ -4,5 +4,23 @@ export const Container = styled.div`
   grid-area: MH;
 
   background-color: ${(props) => props.theme.colors.secondary};
-  color: ${(props) => props.theme.colors.white};
+  
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  
+  padding: 0 10px;
+  border-bottom: 1px solid ${(props) => props.theme.colors.gray};
+  `;
+
+export const Profile = styled.div`
+color: ${(props) => props.theme.colors.white};
+`;
+
+export const Welcome = styled.h3`
+
+`;
+
+export const UserName = styled.span`
+
 `;
