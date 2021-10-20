f6653cb33c79354f83bfd304ca718344b233a50c
Matheus Gomes <gomesprogramacao@gmail.com>
2021-10-20 12:42:08 -0300

    feat: structure and style cotainer base Transactions

--- src/container/transactions/index.tsx
+++ src/container/transactions/index.tsx
@@ -1,11 +1,40 @@
 import React from "react";
 
+// temmplates
+import Aside from "../../templates/Aside";
+import MainHeader from "../../templates/MainHeader";
+import Content from "../../templates/Content";
+
+// components
+import ContentHeader from "../../components/ContentHeader";
+import SelectInput from "../../components/SelectInput";
+
 // styles
-// import { Container } from './styles';
+import { Container } from "./styles";
+import { Grid } from "../shared/styles";
 
 // -------------------------------------------------
 // Export Function
 // -------------------------------------------------
-export const Transactions: React.FC = () => {
-	return <h1>Transactions</h1>;
+export const TransactionsContainer: React.FC = () => {
+	const options = [
+		{
+			value: "Matheus",
+			label: "Matheus",
+		},
+	];
+	return (
+		<Container>
+			<Grid>
+				<MainHeader />
+				<Aside />
+				<Content>
+					<ContentHeader title="Lista" lineColor="#4E41F0">
+						<SelectInput options={options} />
+						<SelectInput options={options} />
+					</ContentHeader>
+				</Content>
+			</Grid>
+		</Container>
+	);
 }
