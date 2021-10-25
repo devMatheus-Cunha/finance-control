e078563061111b5394859562a1407994b959bc34
Matheus Gomes <gomesprogramacao@gmail.com>
2021-10-25 13:13:15 -0300

    feat: add style to scrollbar

--- src/templates/Content/styles.ts
+++ src/templates/Content/styles.ts
@@ -7,4 +7,21 @@ export const Container = styled.div`
   color: ${(props) => props.theme.colors.white};
 
   padding: 1.5rem 1.8rem;
+
+  height: calc(100vh - 70px);
+
+  overflow-y: scroll;
+
+  ::-webkit-scrollbar {
+    width: 10px;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background-color: ${({ theme }) => theme.colors.secondary};
+    border-radius: 10px;
+  }
+
+  ::-webkit-scrollbar-track {
+    background-color: ${({ theme }) => theme.colors.tertiary};
+  }
 `;
