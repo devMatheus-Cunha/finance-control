cebfd416b9e45334a23eedfafb0c87a7b614dd74
Matheus Gomes <gomesprogramacao@gmail.com>
2021-10-28 11:30:27 -0300

    feat: add functions to formate date and currency

---
+++ src/utils/functions.ts
@@ -0,0 +1,23 @@
+export const formatCurrency = (currency: number): string => {
+	const currencyFormated = new Intl.NumberFormat("pt-BR", {
+		style: "currency",
+		currency: "BRL",
+	}).format((currency || 0) as number);
+
+	return currencyFormated;
+};
+
+export const formatDate = (date: string): string => {
+	const dateFormated = new Date(date);
+	const day = dateFormated.getDate() >= 9
+    	? dateFormated.getDate() + 1
+    	: `0${dateFormated.getDate() + 1}`;
+
+	const month = dateFormated.getMonth() > 9
+    	? dateFormated.getMonth() + 1
+    	: `0${dateFormated.getMonth() + 1}`;
+
+	const year = dateFormated.getFullYear();
+
+	return `${day}/${month}/${year}`;
+};
