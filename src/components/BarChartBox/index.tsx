3583a36fc08131af82a99dee6664258f8426bd97
devMatheus-Cunha <devmatheusgr@gmail.com>
2021-11-23 15:16:49 -0300

    feat: grafic base for component BarChartBox

--- src/components/BarChartBox/index.tsx
+++ src/components/BarChartBox/index.tsx
@@ -1,14 +1,50 @@
 import React from "react";
+import {
+	ResponsiveContainer, BarChart, Bar, Cell, Tooltip,
+} from "recharts";
+
+// utils
+import { formatCurrency } from "../../utils/functions";
 
 // styles
-import { Container } from "./styles";
+import { Container, SideLeft, SideRight } from "./styles";
 
-const BarChartBox: React.FC = () => {
+interface IBarChartProps {
+  title: string;
+  data: {
+    amout: number;
+    color: string;
+    name: string;
+    percent: number;
+  }[];
+}
+
+const BarChartBox = ({ title, data }: IBarChartProps) => {
 	return (
 		<Container>
-			Oi
+			<SideLeft>
+				<h2>{title}</h2>
+			</SideLeft>
+
+			<SideRight>
+				<ResponsiveContainer>
+					<BarChart data={data}>
+						<Bar dataKey="amout">
+							{data.map((indicator) => (
+								<Cell
+									key={indicator.name}
+									fill={indicator.color}
+								/>
+							))}
+						</Bar>
+						<Tooltip
+							formatter={(value: string) => formatCurrency(Number(value))}
+						/>
+					</BarChart>
+				</ResponsiveContainer>
+			</SideRight>
 		</Container>
 	);
-}
+};
 
 export default BarChartBox;
--- src/components/BarChartBox/styles.ts
+++ src/components/BarChartBox/styles.ts
@@ -1,5 +1,26 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  width: 47%;
+  min-height: 260px;
+
+  margin: 10px 0;
   
+  background-color: ${({ theme }) => theme.colors.tertiary};
+  color: ${({ theme }) => theme.colors.white};
+  border-radius: 7px;
+`;
+
+export const SideLeft = styled.aside`
+  padding: 30px 20px;
+
+  h2 {
+    margin-bottom: 10px;
+    padding-left: 16px ;
+  }
+`;
+
+export const SideRight = styled.main`
+  flex: 1;
+  height: 150px;
 `;
