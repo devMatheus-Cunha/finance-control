8f4b0c33364a05b98764a785a70f262d08209275
devMatheus-Cunha <devmatheusgr@gmail.com>
2021-11-23 16:07:39 -0300

    feat: gafric and legend to again and expenses for recurring and eventual

--- src/components/BarChartBox/index.tsx
+++ src/components/BarChartBox/index.tsx
@@ -7,7 +7,9 @@ import {
 import { formatCurrency } from "../../utils/functions";
 
 // styles
-import { Container, SideLeft, SideRight } from "./styles";
+import {
+	Container, LegendContainer, SideLeft, SideRight, Legend,
+} from "./styles";
 
 interface IBarChartProps {
   title: string;
@@ -24,6 +26,19 @@ const BarChartBox = ({ title, data }: IBarChartProps) => {
 		<Container>
 			<SideLeft>
 				<h2>{title}</h2>
+				<LegendContainer>
+					{
+						data.map((indicator) => (
+							<Legend key={indicator.name} color={indicator.color}>
+								<div>
+									{indicator.percent}
+									%
+								</div>
+								<span>{indicator.name}</span>
+							</Legend>
+						))
+					}
+				</LegendContainer>
 			</SideLeft>
 
 			<SideRight>
@@ -37,9 +52,6 @@ const BarChartBox = ({ title, data }: IBarChartProps) => {
 								/>
 							))}
 						</Bar>
-						<Tooltip
-							formatter={(value: string) => formatCurrency(Number(value))}
-						/>
 					</BarChart>
 				</ResponsiveContainer>
 			</SideRight>
--- src/components/BarChartBox/styles.ts
+++ src/components/BarChartBox/styles.ts
@@ -1,11 +1,17 @@
 import styled from "styled-components";
 
+interface ILegendProps {
+  color: string;
+}
+
 export const Container = styled.div`
-  width: 47%;
-  min-height: 260px;
+  width: 48%;
+  height: 260px;
+
+  display: flex;
 
   margin: 10px 0;
-  
+
   background-color: ${({ theme }) => theme.colors.tertiary};
   color: ${({ theme }) => theme.colors.white};
   border-radius: 7px;
@@ -13,14 +19,70 @@ export const Container = styled.div`
 
 export const SideLeft = styled.aside`
   padding: 30px 20px;
+  flex: 1;
 
   h2 {
     margin-bottom: 10px;
-    padding-left: 16px ;
+    padding-left: 16px;
+  }
+`;
+
+export const LegendContainer = styled.ul`
+  list-style: none;
+
+  max-height: 180px;
+  padding: 5px 15px 5px 5px;
+
+  align-self: flex-end;
+
+  overflow-y: scroll;
+
+  ::-webkit-scrollbar {
+    width: 10px;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background-color: ${({ theme }) => theme.colors.secondary};
+    border-radius: 10px;
+  }
+
+  ::-webkit-scrollbar-track {
+    background-color: ${({ theme }) => theme.colors.tertiary};
+  }
+`;
+
+export const Legend = styled.li<ILegendProps>`
+  display: flex;
+  align-items: center;
+
+  padding-left: 16px;
+
+  margin-bottom: 7px;
+  font-size: 14px;
+
+  > div {
+    background-color: ${({ color }) => color};
+
+    width: 42px;
+    height: 42px;
+    border-radius: 7px;
+
+    font-size: 14.5px;
+    text-align: center;
+    line-height: 42px;
+  }
+
+  > span {
+    margin-left: 5px;
   }
 `;
 
 export const SideRight = styled.main`
   flex: 1;
-  height: 150px;
+  min-height: 150px;
+
+  display: flex;
+  justify-content: center;
+
+  padding-top: 35px;
 `;
